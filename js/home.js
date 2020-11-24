a47474498115831541afc87aff0980f7af60f496
beni2412 <hardaman97@gmail.com>
2020-11-24 08:53:34 +0530

    [Hardaman] Refactored: To add multiple employees form JSON Object

--- js/home.js
+++ js/home.js
@@ -4,24 +4,59 @@ window.addEventListener("DOMContentLoaded", (event) => {
  
  const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>"
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`
+    let employeePayrollList = createEmployeePayrollJSON();
+    for(const employee of employeePayrollList){
+    innerHtml = `${innerHtml}
     <tr>
     <td>
-        <img class="profile" alt="" src="../assets/profile-images/profile3.jpg" width="50px">
+        <img class="profile" alt="" src="${employee._picture}" width="50px">
      </td>
-     <td>Hardaman Singh</td>
-       <td>Male</td>
+     <td>${employee._name}</td>
+     <td>${employee._gender}</td>
+     <td>${getDeptHtml(employee._department)}</td>
+     <td>${employee._salary}</td>
+     <td>${employee._startDate}</td>
      <td>
-        <div class="dept-label">Sales</div>
-        <div class="dept-label">Finance</div>
-     </td>
-     <td>50000</td>
-     <td>16 Sept 2020</td>
-     <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="../assets/images/delete.jpg" width="30px">
-        <img id="1" alt="edit" onclick="update(this)" src="../assets/images/edit.jpg" width="30px">
+         <img id="${employee._id}" onclick="remove(this)" alt="delete" src="../assets/images/delete.jpg" width="30px">
+         <img id="${employee._id}" alt="edit" onclick="update(this)" src="../assets/images/edit.jpg" width="30px">
      </td>
     </tr>
     `;
+    }
     document.querySelector("#display").innerHTML = innerHtml;
+ }; 
+
+ const createEmployeePayrollJSON = () => {
+    let employeeListLocal = [
+        {
+        _name: "Hardaman",
+        _gender: "Male",
+        _department: ["Finance"],
+        _salary: "50000",
+        _startDate: "16 Sep 2020",
+        _note: "",
+        _id: new Date().getTime(),
+        _picture: "../assets/profile-images/profile3.jpg" 
+        },
+    {
+     _name: "Aman",
+     _gender: "Female",
+     _department: ["HR","Sales"],
+     _salary: "70000",
+     _startDate: "11 Nov 2018",
+     _note: "",
+     _id: new Date().getTime() + 1,
+     _picture: "../assets/profile-images/profile1.jpg" 
+    }
+    ];
+    return employeeListLocal;
+ };
+ 
+ const getDeptHtml = (deptList) => {
+     let deptHtml = "";
+     for(const dept of deptList){
+         deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`
+     }
+     return deptHtml;
  }; 
